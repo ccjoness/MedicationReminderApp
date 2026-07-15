9dd16cddaa92926b7b02616775a0bc41ddbfa872
Chris Jones <chris.charles.jones@gmail.com>
2026-07-15 15:15:01 -0700

    added config options in babel.config.js to enable web

--- MedReminder/babel.config.js
+++ MedReminder/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    "presets": [
+      ["babel-preset-expo", {
+      "unstable_transformImportMeta": true
+    }]
+    ]
     // Note: react-native-reanimated/plugin is intentionally NOT listed here.
     // In Reanimated v4 (SDK 54+), babel-preset-expo handles the plugin automatically.
   };
